28ca5304361da3ba05d69e4c279a7d4a61934ac3
Gorkem Can Ates <76211399+gorkemcanates@users.noreply.github.com>
2023-03-10 12:36:56 -0500

    Update double_unet.py

--- model/double_unet.py
+++ model/double_unet.py
@@ -38,11 +38,11 @@ class DoubleUnet(nn.Module):
                 out_features=3, 
                 k=1,
                 input_size=(512, 512),
-                patch_size_ratio=8,
+                patch_size=8,
                 spatial_att=True,
                 channel_att=True,
-                spatial_head_dim=8,
-                channel_head_dim=8, 
+                spatial_head_dim=[4, 4, 4, 4],
+                channel_head_dim=[1, 1, 1, 1],
                 device='cuda', 
                 ) -> None:
         super().__init__()
@@ -50,7 +50,7 @@ class DoubleUnet(nn.Module):
             torch.cuda.set_enabled_lms(True)
 
         self.attention = attention    
-        patch_size = input_size[0] // patch_size_ratio
+        patch = input_size[0] // patch_size
   
         pretrained = False
 
@@ -82,8 +82,8 @@ class DoubleUnet(nn.Module):
         if self.attention:
             self.DCA_vgg1 = DCA(n=n,                                            
                                     features = [64, 128, 256, 512],                                                                                                              
-                                    strides=[patch_size_ratio, patch_size_ratio // 2, patch_size_ratio // 4, patch_size_ratio // 8],
-                                    patch_size=patch_size,
+                                    strides=[patch_size, patch_size // 2, patch_size // 4, patch_size // 8],
+                                    patch=patch,
                                     spatial_att=spatial_att,
                                     channel_att=channel_att, 
                                     spatial_head=spatial_head_dim,
