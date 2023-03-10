d992361bd6a5ea7b2db15ea588a98441327449d3
Gorkem Can Ates <76211399+gorkemcanates@users.noreply.github.com>
2023-03-10 12:36:08 -0500

    Update multi_res_unet.py

--- model/multi_res_unet.py
+++ model/multi_res_unet.py
@@ -108,11 +108,11 @@ class MultiResUnet(nn.Module):
                 out_features=3, 
                 k=1,
                 input_size=(512, 512),
-                patch_size_ratio=8,
+                patch_size=8,
                 spatial_att=True,
                 channel_att=True,
-                spatial_head_dim=8,
-                channel_head_dim=8, 
+                spatial_head_dim=[4, 4, 4, 4],
+                channel_head_dim=[1, 1, 1, 1],
                 device='cuda', 
                 ) -> None:
         super().__init__()
@@ -162,8 +162,8 @@ class MultiResUnet(nn.Module):
         if self.attention:
             self.DCA = DCA(n=n,                                            
                                 features = [int(32 * k), int(64 * k), int(128 * k), int(256 * k)],                                                                                                              
-                                strides=[patch_size_ratio, patch_size_ratio // 2, patch_size_ratio // 4, patch_size_ratio // 8],
-                                patch_size=patch_size,
+                                strides=[patch_size, patch_size // 2, patch_size // 4, patch_size // 8],
+                                patch=patch,
                                 spatial_att=spatial_att,
                                 channel_att=channel_att, 
                                 spatial_head=spatial_head_dim,
